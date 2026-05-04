c55d59e51e4ee0fa122d6ff35952572e0fe354b4
surya pratap <suryaraghav2703@gmail.com>
2026-05-04 16:51:54 +0530

    Add files via upload

---
+++ JavaScript/Ch-7(Walking with DOM)/basic_selectors.js
@@ -0,0 +1,19 @@
+console.log("surya")
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+
+// boxes[2].style.backgroundColor = "red"
+
+// document.getElementById("redbox").style.backgroundColor = "red"
+
+// document.querySelector(".box").style.backgroundColor = "green" 
+
+// console.log(document.querySelectorAll(".box"))
+
+// document.querySelectorAll(".box").forEach(e =>{
+//     e.style.backgroundColor = "green";
+// })
+
+// document.getElementsByTagName("div")
+
