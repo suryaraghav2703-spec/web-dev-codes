e33af4c3b43ecb7e7f817054a5c9956e37e4ff6c
surya pratap <suryaraghav2703@gmail.com>
2026-05-07 22:15:43 +0530

    Add files via upload

---
+++ JavaScript/Task/changing_content&style.js
@@ -0,0 +1,8 @@
+document.getElementById("info").innerText = "Hello, user!";
+
+document.getElementById("box").innerHTML = "<strong>Welcome!</strong>";
+
+document.getElementById("heading").textContent = "Welcome to the site!"
+
+document.getElementById("box").style.color = "blue";
+document.getElementById("box").style.backgroundColor = "lightgrey";
