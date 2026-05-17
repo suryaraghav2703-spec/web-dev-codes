fccaee8dfd284f55564a10038ac6ab7fc9b93f5b
surya pratap <suryaraghav2703@gmail.com>
2026-05-17 22:25:17 +0530

    Add files via upload

---
+++ JavaScript/Task/Synchronous.js
@@ -0,0 +1,8 @@
+console.log("step 1: boil water (take 5 minutes)");
+for(let i = 0; i < 19999999999; i++){}
+console.log("step 2: add maggie noodles");
+console.log("step 3: stir and cook wait for 2 minutes....");
+for(let i = 0; i < 19999999999; i++){}
+console.log("step 4: add masala");
+for(let i = 0; i < 19999999999; i++){}
+console.log("step 5: serve hot and enjoy your meal");
