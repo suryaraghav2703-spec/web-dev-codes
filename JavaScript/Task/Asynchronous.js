577f266c5242c2e78a865ea921eb1474d88e36dc
surya pratap <suryaraghav2703@gmail.com>
2026-05-19 23:31:25 +0530

    Add files via upload

---
+++ JavaScript/Task/Asynchronous.js
@@ -0,0 +1,12 @@
+console.log("starting to make maggie..");
+setTimeout(()=>{
+    console.log('maggie is ready!')
+},5000);
+
+setTimeout(()=>{
+    console.log('watching my favorite cartoon')
+},3000);
+
+setTimeout(()=>{
+    console.log('talking to my friend on phone')
+},2000);
