165247f39fe60a3858737dbbe54aaf037997cafe
surya pratap <suryaraghav2703@gmail.com>
2026-05-26 08:58:24 +0530

    Add files via upload

---
+++ JavaScript/Task/setInterval.js
@@ -0,0 +1,11 @@
+setInterval(()=>{
+    console.log("⏰ Reminder: Drink Water!");
+},3000);
+
+
+console.log("starting the interval...");
+setInterval(()=>{
+    console.log("Repeating Message");
+},2000);
+
+console.log("interval has been set.");
