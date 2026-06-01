e161a2c8ee0038752561081da60fc71095d84dcb
surya pratap <suryaraghav2703@gmail.com>
2026-06-01 21:44:39 +0530

    Delete JavaScript/Questions/q.6).js

--- JavaScript/Questions/q.6).js
+++
@@ -1,5 +0,0 @@
-const form = document.getElementById("myform");
-form.addEventListener('submit', function(event){
-     event.preventDefault(); // prevent the default form submission
-     alert("Form submitted!");
-});
