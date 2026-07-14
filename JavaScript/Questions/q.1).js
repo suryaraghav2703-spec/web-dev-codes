fa32c31b31884b1e28e82df31c66521367750ddc
surya pratap <suryaraghav2703@gmail.com>
2026-07-14 22:30:28 +0530

    Delete JavaScript/Questions/q.1).js

--- JavaScript/Questions/q.1).js
+++
@@ -1,8 +0,0 @@
-// 1. select both element
-const statusText = document.getElementById('statue-text');
-const actionButton = document.getElementById('action-button');
-// 2. Add the event Lsitener to the button
-actionButton.addEventListener('click', () =>{
-    // 3. his callback function runs on click
-    statusText.textContent = "Button was clicked!";
-});
