3b0e91090fbd8e371d004cc7b38d00a8fc958888
surya pratap <suryaraghav2703@gmail.com>
2026-07-15 22:15:41 +0530

    Delete JavaScript/Questions/q.2).js

--- JavaScript/Questions/q.2).js
+++
@@ -1,6 +0,0 @@
-// 1. select the element by its Id
-const textElement = document.getElementById('main-text');
-
-// 2. change its style properties
-textElement.style.color = 'red';
-textElement.style.font = '24px';
