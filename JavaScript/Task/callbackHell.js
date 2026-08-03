bc67e85fdd6d316658aa217d40153e797702b1ef
surya pratap <suryaraghav2703@gmail.com>
2026-08-03 23:08:49 +0530

    Add files via upload

---
+++ JavaScript/Task/callbackHell.js
@@ -0,0 +1,17 @@
+console.log("Starting homework...");
+
+setTimeout(() => {
+
+console.log("Homework done!"); console.log("Starting dinner...");
+
+setTimeout(() => {
+
+console.log("Dinner done!"); console.log("Getting ready to go out...");
+
+setTimeout(() => {
+
+console.log("Going to the playground!"); }, 1000); // after dinner
+
+}, 1500); // dinner time
+
+}, 2000); // homework time
