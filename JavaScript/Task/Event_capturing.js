43e955e3c1bf79532a20386affbc0b711d09c7ff
surya pratap <suryaraghav2703@gmail.com>
2026-09-17 21:27:06 +0530

    Delete JavaScript/Task/Event_capturing.js

--- JavaScript/Task/Event_capturing.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("outer").addEventListener("click", () => {
-alert("Outer Div (Capturing)"); 
- }, true); // true = capturing phase
-document.getElementById("inner").addEventListener("click", () => {
-alert("Inner Div (Capturing)");
-}, true); // true capturing phase
-document.getElementById("btn").addEventListener("click", () => {
- alert("Button Clicked");
-}); 
